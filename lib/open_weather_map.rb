3aa1bef03c643fac3cd0dae7eb9bfda8c152d1c8
tse <markus@herrthese.de>
2017-08-13 14:48:19 +0200

    new api key

--- lib/open_weather_map.rb
+++ lib/open_weather_map.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 
 class OpenWeatherMap
 
-  HAMBURG = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0'
+  HAMBURG = 'http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1'
 
   def current_weather
     JSON.parse(open(HAMBURG).read)
